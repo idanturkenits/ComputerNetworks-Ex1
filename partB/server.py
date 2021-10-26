d3d02487a91377f006947a27d812a00e67bcb313
MaZeChpatCha <danielputter70@gmail.com>
2021-10-26 22:00:11 +0300

    Simplified the code.

--- partB/server.py
+++ partB/server.py
@@ -4,32 +4,19 @@ from client import HEADER_SIZE, PACKET_SIZE
 
 
 def main():
-    # setting up port and socket
     my_port = int(argv[1])
     s = socket(AF_INET, SOCK_DGRAM)
     s.bind(('', my_port))
-    # list to keep track of the packets we printed
-    printed = []
-    # list to  keep track of the packets we got
-    seen = dict()
-    i = 1
+    seen, printed, package_number = dict(), [], 0
     while True:
-        # getting a packet from the client
         data, source_addr = s.recvfrom(PACKET_SIZE)
         non_binary_data = data.decode('utf-8')
         place = int(non_binary_data[:HEADER_SIZE])
-        # adding the data to the seen dictunary
         seen[place] = non_binary_data[HEADER_SIZE:]
-
-        # if we have a packet with the packet
-        # number that matches the order, and we didnt print it,
-        # we print it and add it to the printed list
-        while i in seen and i not in printed:
-            print(seen[i], end="", flush=True)
-            printed.append(i)
-            i = i + 1
-
-        # send the data back to the client
+        while package_number in seen.keys() and package_number not in printed:
+            print(seen[package_number], end="", flush=True)
+            printed.append(package_number)
+            package_number = package_number + 1
         s.sendto(data, source_addr)
 
 
