cda86d9932b9614caecb3612b17337bac2828294
MaZeChpatCha <danielputter70@gmail.com>
2021-10-29 18:28:37 +0300

    Added input validation.

--- partB/client.py
+++ partB/client.py
@@ -1,18 +1,18 @@
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import socket, AF_INET, SOCK_DGRAM, error
 from sys import argv
 
 PACKET_SIZE, CHUNK_SIZE, HEADER_SIZE = 100, 90, 10
 
 
 def send_and_get_returned_file(addr, file):
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.settimeout(1)
-    with open(file) as f:
+    try:
+        f = open(file)
         contents = f.read()
-    # a variable to store the text that the server returned - unnecessary
-    # keeping track of how many packets we've got - length of seen_package_nums
-    # keeping track of the number of packet we send - i/CHUNK_SIZE can do that
-    # keeping track of which packet numbers we've got
+        f.close()
+    except FileNotFoundError:
+        raise FileNotFoundError("Couldn't find file.")
+    s = socket(AF_INET, SOCK_DGRAM)
+    s.settimeout(0.25)
     seen_package_nums = []
     for i in range(0, len(contents), CHUNK_SIZE):
         chunk, packet_number = contents[i:i + CHUNK_SIZE], i // CHUNK_SIZE
@@ -29,9 +29,14 @@ def send_and_get_returned_file(addr, file):
 
 
 def main():
-    dest_addr = (argv[2], int(argv[1]))
-    file_path = argv[3]
-    send_and_get_returned_file(dest_addr, file_path)
+    try:
+        if len(argv) != 4:
+            raise ValueError(f'Exactly 3 arguments are necessary, {len(argv) - 1} were inserted.')
+        dest_addr = (argv[2], int(argv[1]))
+        file_path = argv[3]
+        send_and_get_returned_file(dest_addr, file_path)
+    except (FileNotFoundError, ValueError, error) as e:
+        print(e)
 
 
 if "__main__" == __name__:
